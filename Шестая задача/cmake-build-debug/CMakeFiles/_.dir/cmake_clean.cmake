c69e9d6537498f652fb741adecd3bb89904ba9e1
Alexsej1 <112761694+Alexsej1@users.noreply.github.com>
2023-07-04 00:21:02 +0300

    Add files via upload

---
+++ Шестая задача/cmake-build-debug/CMakeFiles/_.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_.dir/main.cpp.obj"
+  "CMakeFiles/_.dir/main.cpp.obj.d"
+  "_.exe"
+  "_.exe.manifest"
+  "_.pdb"
+  "lib_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Шестая задача/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::ifstream infile("input.txt");
+    std::ofstream outfile("output.txt");
+
+    if (!infile.is_open() || !outfile.is_open()) {
+        std::cout << "Error opening file(s)" << std::endl;
+        return 1;
+    }
+
+    std::string text;
+    std::getline(infile, text);
+
+    std::vector<std::string> sentences;
+    std::string delimiters = ".!";
+    size_t start = 0;
+    size_t end = text.find_first_of(delimiters);
+
+    while (end != std::string::npos) {
+        sentences.push_back(text.substr(start, end - start + 1));
+        start = end + 1;
+        end = text.find_first_of(delimiters, start);
+    }
+
+    if (start < text.length()) {
+        sentences.push_back(text.substr(start));
+    }
+
+    for (const auto& sentence : sentences) {
+        std::vector<std::string> words;
+        std::string delimiter = " ";
+        size_t pos = 0;
+        size_t last_pos = 0;
+
+        while ((pos = sentence.find(delimiter, last_pos)) != std::string::npos) {
+            std::string word = sentence.substr(last_pos, pos - last_pos);
+            words.push_back(word);
+            last_pos = pos + delimiter.length();
+        }
+
+        std::string last_word = sentence.substr(last_pos);
+        words.push_back(last_word);
+
+        std::string longest_word = "";
+        for (const auto& word : words) {
+            if (word.length() > longest_word.length()) {
+                longest_word = word;
+            }
+        }
+
+        outfile << longest_word << std::endl;
+    }
+
+    infile.close();
+    outfile.close();
+
+    return 0;
+}
